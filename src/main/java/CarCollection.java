1fdffdb3da39fa2106fe49c981d3b65086b29f8f
JungwooSim <cscd053@gmail.com>
2021-04-30 00:21:06 +0900

    feat : 경기에 참여하는 자동차들을 갖는 객체 추가

---
+++ src/main/java/CarCollection.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarCollection {
+    private List<Car> carCollection;
+
+    public CarCollection() {
+        this.carCollection = new ArrayList<>();
+    }
+
+    public List<Car> getCarCollection() {
+        return carCollection;
+    }
+}
